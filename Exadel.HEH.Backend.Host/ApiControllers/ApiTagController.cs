7bcc513d75cb826c472204e1665069639596fdc5
KonstantinMarchenko <k_mark99@mail.ru>
2021-01-19 21:30:04 +0300

    Added tag and user controllers

---
+++ Exadel.HEH.Backend.Host/ApiControllers/ApiTagController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Exadel.HEH.Backend.DataAccess.Models;
+using Exadel.HEH.Backend.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exadel.HEH.Backend.Host
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiTagController
+    {
+        private readonly TagRepository _tagRepository;
+
+        public ApiTagController(TagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<Tag>> Get()
+        {
+            return _tagRepository.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public Task<Tag> Get(Guid id)
+        {
+            var tag = _tagRepository.GetByIdAsync(id);
+
+            return tag == null ? null : _tagRepository.GetByIdAsync(id);
+        }
+
+        [HttpPost]
+        public Task Post(Tag tag)
+        {
+            return _tagRepository.CreateAsync(tag);
+        }
+
+        [HttpPut("{id}")]
+        public Task<Tag> Put(Guid id, Tag tagInsert)
+        {
+            var tag = _tagRepository.GetByIdAsync(id);
+
+            if (tag == null)
+            {
+                return null;
+            }
+
+            _tagRepository.UpdateAsync(id, tagInsert);
+
+            return _tagRepository.GetByIdAsync(id);
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(Guid id)
+        {
+            var tag = _tagRepository.GetByIdAsync(id);
+
+            if (tag == null)
+            {
+                return null;
+            }
+
+            _tagRepository.RemoveAsync(id);
+
+            return "Deleted";
+        }
+    }
+}
---
+++ Exadel.HEH.Backend.Host/ApiControllers/ApiUserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Exadel.HEH.Backend.DataAccess.Models;
+using Exadel.HEH.Backend.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exadel.HEH.Backend.Host
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiUserController
+    {
+        private readonly UserRepository _userRepository;
+
+        public ApiUserController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<User>> Get()
+        {
+            return _userRepository.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public Task<User> Get(Guid id)
+        {
+            var user = _userRepository.GetByIdAsync(id);
+
+            return user == null ? null : _userRepository.GetByIdAsync(id);
+        }
+
+        [HttpPut("{id}")]
+        public Task<User> Put(Guid id, User userInsert)
+        {
+            var user = _userRepository.GetByIdAsync(id);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            _userRepository.UpdateAsync(id, userInsert);
+
+            return _userRepository.GetByIdAsync(id);
+        }
+    }
+}
