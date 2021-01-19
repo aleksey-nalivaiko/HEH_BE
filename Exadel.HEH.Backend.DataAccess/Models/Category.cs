5ca00594dc739ca880410468e6c4081f1f8da657
YuriPetruchenia <yourockpet@gmail.com>
2021-01-19 19:12:25 +0300

    Modified Models/Category.cs, Models/Discount.cs, Models/History.cs, Models/PreOrder.cs

--- Exadel.HEH.Backend.DataAccess/Models/Category.cs
+++ Exadel.HEH.Backend.DataAccess/Models/Category.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Exadel.HEH.Backend.DataAccess.Models
 {
     public class Category : IDataModel
     {
+        [BsonId]
         public Guid Id { get; set; }
 
+        [BsonElement("name")]
         public string Name { get; set; }
     }
 }
--- Exadel.HEH.Backend.DataAccess/Models/Discount.cs
+++ Exadel.HEH.Backend.DataAccess/Models/Discount.cs
@@ -1,25 +1,35 @@
 ﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Exadel.HEH.Backend.DataAccess.Models
 {
     public class Discount
     {
+        [BsonId]
         public Guid Id { get; set; }
 
+        [BsonElement("conditions")]
         public string Conditions { get; set; }
 
+        [BsonElement("tagsIds")]
         public Tag[] Tags { get; set; }
 
+        [BsonElement("vendorId")]
         public Guid VendorId { get; set; }
 
+        [BsonElement("promoCode")]
         public string PromoCode { get; set; }
 
+        [BsonElement("adresses")]
         public Address[] Addresses { get; set; }
 
+        [BsonElement("stratDate")]
         public DateTime StartDate { get; set; }
 
+        [BsonElement("endDate")]
         public DateTime EndDate { get; set; }
 
+        [BsonElement("categoryId")]
         public Guid CategoryId { get; set; }
     }
 }
