9bc3630a1e63b57309e29c3ee9a2a06ab2504840
YuriPetruchenia <yourockpet@gmail.com>
2021-01-19 11:58:50 +0300

    Add 3 Models/Category.cs Models/PreOrder.cs Models/Tag.cs

--- Exadel.HEH.Backend.BusinessLogic.Tests/Class1.cs
+++ Exadel.HEH.Backend.BusinessLogic.Tests/Class1.cs
@@ -4,5 +4,8 @@ namespace Exadel.HEH.Backend.BusinessLogic.Tests
 {
     public class Class1
     {
+        public int MyProperty { get; set; }
+
+        public int qweqwr { get; set; }
     }
 }
--- Exadel.HEH.Backend.BusinessLogic/Class1.cs
+++ Exadel.HEH.Backend.BusinessLogic/Class1.cs
@@ -4,5 +4,6 @@ namespace Exadel.HEH.Backend.BusinessLogic
 {
     public class Class1
     {
+        public int qewqw_qwq { get; set; }
     }
 }
