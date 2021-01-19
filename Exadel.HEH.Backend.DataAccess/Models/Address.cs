181b382def3e1c06b19c5fb3aa18cca0b8abe56c
elizavetapavliv <pavliv.liza@yandex.ru>
2021-01-19 12:41:45 +0300

    Add User and History models

---
+++ Exadel.HEH.Backend.DataAccess/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exadel.HEH.Backend.DataAccess.Models
+{
+    public class Address
+    {
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+    }
+}
---
+++ Exadel.HEH.Backend.DataAccess/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exadel.HEH.Backend.DataAccess.Models
+{
+    public class User
+    {
+        public enum UserRole
+        {
+            Employee,
+            Moderator,
+            Administrator
+        }
+
+        public Guid Id { get; set; }
+
+        public UserRole Role { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public Address Office { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Guid[] CategoryNotificationsId { get; set; }
+
+        public Guid[] TagNotificationsId { get; set; }
+
+        public Guid[] VendorNotificationsId { get; set; }
+
+        public bool NewVendorNotificationIsOn { get; set; }
+
+        public bool NewDiscountNotificationIsOn { get; set; }
+
+        public bool HotDiscountsNotificationIsOn { get; set; }
+
+        public bool CityChangeNotificationIsOn { get; set; }
+
+        public Favorites[] Favorites { get; set; }
+    }
+}
