25282cde24ae4ca72f29ed2d61cbf2e8ad58e834
elizavetapavliv <pavliv.liza@yandex.ru>
2021-01-19 18:30:53 +0300

    Split script into multiple files

---
+++ Exadel.HEH.Backend.Host/Deployment/DBScripts/discountData.js
@@ -0,0 +1,41 @@
+﻿let discountData = [
+    {
+        "_id": UUID("06734050-5268-4540-91d0-8a4e9a7af6a0"),
+        "conditions": "Conditions string",
+        "tagsIds": [
+            UUID("8134e0e7-bd6f-4aa2-82de-b6301fd2979f"),
+            UUID("81cf086a-84a8-4291-9a55-791f1005f09e")
+        ],
+        "vendorId": UUID("662c27f1-8f63-44ac-a873-aac6f09ab173"),
+        "promoCode": "promo code 1",
+        "addresses": [
+            UUID("a009f99e-5a5d-11eb-ae93-0242ac130002"),
+            UUID("9cc9a572-5a5d-11eb-ae93-0242ac130002")
+        ],
+        "phones": [
+            UUID("a7feedac-5a5e-11eb-ae93-0242ac130002")
+        ],
+        "startDate": new Date("2021-01-18"),
+        "endDate": new Date("2021-01-22"),
+        "categoryId": UUID("fb42cd13-1c15-4388-bb3d-216376b5a225")
+    },
+    {
+        "_id": UUID("5ff9732f-8187-4b14-92d7-89bd72cd91ba"),
+        "conditions": "Conditions string",
+        "tagsIds": [
+            UUID("8134e0e7-bd6f-4aa2-82de-b6301fd2979f"),
+            UUID("81cf086a-84a8-4291-9a55-791f1005f09e")
+        ],
+        "vendorId": UUID("ef809bbf-1ff7-4f1c-ac2b-ab5040294cf1"),
+        "promoCode": "promo code 1",
+        "addresses": [
+            UUID("fd40230e-5a5d-11eb-ae93-0242ac130002")
+        ],
+        "phones": [
+            UUID("b614783a-5a5e-11eb-ae93-0242ac130002")
+        ],
+        "startDate": new Date("2021-01-18"),
+        "endDate": new Date("2021-01-22"),
+        "categoryId": UUID("fb42cd13-1c15-4388-bb3d-216376b5a225")
+    }
+];
---
+++ Exadel.HEH.Backend.Host/Deployment/DBScripts/userData.js
@@ -0,0 +1,156 @@
+let userData = [
+  {
+    "_id": UUID("6dead3f8-599e-11eb-ae93-0242ac130002"),
+    "role": "moderator",
+    "name": "Mary Jones",
+    "email": "maryj@exadel.com",
+    "password": "a5Adj7O7",
+    "address": {
+      "country": "Belarus",
+      "city": "Minsk",
+      "street": "street 1"
+    },
+    "isActive": true,
+    "categoryNotifications": [
+      UUID("fb42cd13-1c15-4388-bb3d-216376b5a225"),
+      UUID("a3ab67b9-1a72-48e1-a928-e0dbe0b17a5c"),
+      UUID("c86bec9f-2ef9-4be2-bd21-af14aa0d187f")
+    ],
+    "tagNotifications": [
+      UUID("ab4b5672-e2f6-4935-ba03-58851d9c5991"),
+      UUID("22427389-7127-41b4-9aa4-af2078980cf8")
+    ],
+    "vendorNotifications": [
+      UUID("662c27f1-8f63-44ac-a873-aac6f09ab173")
+    ],
+    "newVendorNotificationIsOn": true,
+    "newDiscountNotificationIsOn": true,
+    "hotDiscountsNotificationIsOn": false,
+    "cityChangeNotificationIsOn": true,
+    "favorites": [
+      {
+        "discountId": UUID("5ff9732f-8187-4b14-92d7-89bd72cd91ba"),
+        "note": "note for discount 1"
+      },
+      {
+        "discountId": UUID("06734050-5268-4540-91d0-8a4e9a7af6a0"),
+        "note": "note for discount 2"
+      }
+    ]
+  },
+  {
+    "_id": UUID("c8fd9ed0-59a1-11eb-ae93-0242ac130002"),
+    "role": "administrator",
+    "name": "Jack Smith",
+    "email": "jacks@exadel.com",
+    "password": "895sdj9O765",
+    "address": {
+      "country": "Belarus",
+      "city": "Minsk",
+      "street": "street 2"
+    },
+    "isActive": true,
+    "categoryNotifications": [
+      UUID("a3ab67b9-1a72-48e1-a928-e0dbe0b17a5c"),
+      UUID("c86bec9f-2ef9-4be2-bd21-af14aa0d187f")
+    ],
+    "tagNotifications": [
+      UUID("81cf086a-84a8-4291-9a55-791f1005f09e"),
+      UUID("ab4b5672-e2f6-4935-ba03-58851d9c5991")
+    ],
+    "vendorNotifications": [
+      UUID("ef809bbf-1ff7-4f1c-ac2b-ab5040294cf1")
+    ],
+    "newVendorNotificationIsOn": true,
+    "newDiscountNotificationIsOn": true,
+    "hotDiscountsNotificationIsOn": false,
+    "cityChangeNotificationIsOn": true,
+    "favorites": [
+      {
+        "discountId": UUID("5ff9732f-8187-4b14-92d7-89bd72cd91ba"),
+        "note": "note for discount 1"
+      },
+      {
+        "discountId": UUID("06734050-5268-4540-91d0-8a4e9a7af6a0"),
+        "note": "note for discount 2"
+      }
+    ]
+  },
+  {
+    "_id": UUID("a7df950e-59a2-11eb-ae93-0242ac130002"),
+    "role": "employee",
+    "name": "Alex Rollings",
+    "email": "alexr@exadel.com",
+    "password": "5875soj9O765",
+    "address": {
+      "country": "Belarus",
+      "city": "Minsk",
+      "street": "street 3"
+    },
+    "isActive": true,
+    "categoryNotifications": [
+      UUID("a3ab67b9-1a72-48e1-a928-e0dbe0b17a5c"),
+      UUID("c86bec9f-2ef9-4be2-bd21-af14aa0d187f")
+    ],
+    "tagNotifications": [
+      UUID("81cf086a-84a8-4291-9a55-791f1005f09e"),
+      UUID("ab4b5672-e2f6-4935-ba03-58851d9c5991")
+    ],
+    "vendorNotifications": [
+      UUID("ef809bbf-1ff7-4f1c-ac2b-ab5040294cf1")
+    ],
+    "newVendorNotificationIsOn": true,
+    "newDiscountNotificationIsOn": true,
+    "hotDiscountsNotificationIsOn": false,
+    "cityChangeNotificationIsOn": true,
+    "favorites": [
+      {
+        "discountId": UUID("5ff9732f-8187-4b14-92d7-89bd72cd91ba"),
+        "note": "note for discount 1"
+      },
+      {
+        "discountId": UUID("06734050-5268-4540-91d0-8a4e9a7af6a0"),
+        "note": "note for discount 2"
+      }
+    ]
+  },
+  {
+    "_id": UUID("662db544-59a4-11eb-ae93-0242ac130002"),
+    "role": "employee",
+    "name": "Alex Rollings",
+    "email": "alexr@exadel.com",
+    "password": "5875soj9O765",
+    "address": {
+      "country": "Belarus",
+      "city": "Minsk",
+      "street": "street 4"
+    },
+    "isActive": false,
+    "categoryNotifications": [
+      UUID("a3ab67b9-1a72-48e1-a928-e0dbe0b17a5c"),
+      UUID("c86bec9f-2ef9-4be2-bd21-af14aa0d187f")
+    ],
+    "tagNotifications": [
+      UUID("81cf086a-84a8-4291-9a55-791f1005f09e"),
+      UUID("ab4b5672-e2f6-4935-ba03-58851d9c5991")
+    ],
+    "vendorNotifications": [
+      UUID("ef809bbf-1ff7-4f1c-ac2b-ab5040294cf1")
+    ],
+    "newVendorNotificationIsOn": true,
+    "newDiscountNotificationIsOn": true,
+    "hotDiscountsNotificationIsOn": false,
+    "cityChangeNotificationIsOn": true,
+    "favorites": [
+      {
+        "discountId": UUID("5ff9732f-8187-4b14-92d7-89bd72cd91ba"),
+        "note": "note for discount 1"
+      },
+      {
+        "discountId": UUID("06734050-5268-4540-91d0-8a4e9a7af6a0"),
+        "note": "note for discount 2"
+      }
+    ]
+  }
+]
+;
