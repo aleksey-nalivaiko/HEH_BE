0aeab4179df7240721fe9bb569336eb0b2776dc1
YuriPetruchenia <yourockpet@gmail.com>
2021-01-20 18:24:18 +0300

    Add one Service test

---
+++ Exadel.HEH.Backend.BusinessLogic.Tests/CategoryServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Exadel.HEH.Backend.BusinessLogic.Services;
+using Exadel.HEH.Backend.DataAccess.Models;
+using Exadel.HEH.Backend.DataAccess.Repositories.Abstract;
+using Moq;
+using Xunit;
+
+namespace Exadel.HEH.Backend.BusinessLogic.Tests
+{
+    public class CategoryServiceTests
+    {
+        [Fact]
+        public void IndexReturnsAViewResultWithAListOfUsers()
+        {
+            // Arrange
+            var mock = new Mock<ICategoryRepository>();
+            mock.Setup(x => x.GetByTagAsync(Guid.NewGuid())).Returns(GetCategory);
+            var category = new CategoryService(mock.Object);
+
+            // Act
+            var result = new CategoryService(mock.Object);
+
+            // Assert
+            Assert.NotNull(result.GetByTagAsync(Guid.NewGuid()));
+        }
+
+        private Task<Category> GetCategory()
+        {
+            Category category = new Category() { };
+            return Task.FromResult(category);
+        }
+    }
+}
---
+++ Exadel.HEH.Backend.DataAccess.Tests/CategoryRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Exadel.HEH.Backend.DataAccess.Repositories.Abstract;
+using Moq;
+using Xunit;
+
+namespace Exadel.HEH.Backend.DataAccess.Tests
+{
+    public class CategoryRepositoryTests
+    {
+        [Fact]
+        public void SomeName()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
