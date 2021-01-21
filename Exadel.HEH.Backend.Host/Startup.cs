caecf504662865cf046066fc6f43e5af5288f556
elizavetapavliv <pavliv.liza@yandex.ru>
2021-01-21 15:04:28 +0300

    Add Swagger route prefix

--- Exadel.HEH.Backend.Host/Startup.cs
+++ Exadel.HEH.Backend.Host/Startup.cs
@@ -12,7 +12,6 @@ namespace Exadel.HEH.Backend.Host
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
