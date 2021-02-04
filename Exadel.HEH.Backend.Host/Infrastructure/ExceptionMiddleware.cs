3849483d6c8436ec57eefa14e5d1cb4673a2c757
elizavetapavliv <pavliv.liza@yandex.ru>
2021-02-04 14:42:10 +0300

    Add middleware class

---
+++ Exadel.HEH.Backend.Host/Infrastructure/ExceptionMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exadel.HEH.Backend.Host.Infrastructure
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var request = context.Request;
+
+            context.Response.Clear();
+            //context.Response.StatusCode = OkResult;
+
+            await _next(context);
+        }
+
+    }
+}
