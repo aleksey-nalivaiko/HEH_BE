22b809ec2837e4fb05e6e0a5ea95928713249602
elizavetapavliv <pavliv.liza@yandex.ru>
2021-02-05 18:09:46 +0300

    Add new data

--- Exadel.HEH.Backend.Host/Deployment/DBScripts/categoryData.js
+++ Exadel.HEH.Backend.Host/Deployment/DBScripts/categoryData.js
@@ -1,18 +1,34 @@
 ﻿let categoryData = [
     {
-        "_id": UUID("fb42cd13-1c15-4388-bb3d-216376b5a225"),
-        "name": "food"
+        "_id": UUID("ab067429-6fbf-4798-92bf-d62012db8f25"),
+        "name": "Food"
     },
     {
-        "_id": UUID("a3ab67b9-1a72-48e1-a928-e0dbe0b17a5c"),
-        "name": "sport"
+        "_id": UUID("751dc6e4-603b-477f-91dc-97c09d13d96a"),
+        "name": "Sport and Health"
     },
     {
-        "_id": UUID("adbab91e-942b-4bc2-992f-19b6877b1e7d"),
-        "name": "beauty"
+        "_id": UUID("11f8463a-827f-4873-8550-62e0ee03b369"),
+        "name": "Beauty"
     },
     {
-        "_id": UUID("c86bec9f-2ef9-4be2-bd21-af14aa0d187f"),
-        "name": "health"
+        "_id": UUID("8f411964-f934-4302-bdd0-db2fc2a549e7"),
+        "name": "Entertainment"
+    },
+    {
+        "_id": UUID("1c22cbf6-9692-4207-83e3-8d51a6bbd8be"),
+        "name": "Auto"
+    },
+    {
+        "_id": UUID("97c8c966-634a-4a3a-b87b-89d2d31587d3"),
+        "name": "Travel"
+    },
+    {
+        "_id": UUID("66490a95-4e19-4cea-9c60-a12031bd330c"),
+        "name": "Education"
+    },
+    {
+        "_id": UUID("2d9d97e6-5081-492b-8bc2-b817eb6ae85f"),
+        "name": "Other"
     }
-];
+]
--- Exadel.HEH.Backend.Host/Deployment/DBScripts/locationData.js
+++ Exadel.HEH.Backend.Host/Deployment/DBScripts/locationData.js
@@ -2,11 +2,62 @@
     {
         "_id": UUID("0b343598-5efc-11eb-ae93-0242ac130002"),
         "country": "Belarus",
-        "cities": [
-            {
-                "_id": UUID("3075aada-5efc-11eb-ae93-0242ac130002"),
-                "name": "Minsk"
-            }
-        ]
+        "cities":
+            [
+                {
+                    "_id": UUID("021e3664-df32-43f1-8851-f1449cfbc3dd"),
+                    "name": "Minsk"
+                },
+                {
+                    "_id": UUID("a1d19bb5-ba10-4f51-8eac-053a264d8ce2"),
+                    "name": "Brest"
+                },
+                {
+                    "_id": UUID("c77e4c89-57f5-4204-8899-af47545cca22"),
+                    "name": "Mogilev"
+                },
+                {
+                    "_id": UUID("8d766cdc-1cc8-4af8-9af7-007a49fdb026"),
+                    "name": "Gomel"
+                },
+                {
+                    "_id": UUID("612c1628-f2b7-4553-9232-00bfcd19f95f"),
+                    "name": "Grodno"
+                },
+                {
+                    "_id": UUID("91268c45-0b69-40b8-8c03-bab2fdc5f431"),
+                    "name": "Vitebsk"
+                }
+            ]
+    },
+    {
+        "_id": UUID("571ffd86-ae90-4457-b1ec-0ed504076706"),
+        "country": "Ukraine",
+        "cities":
+            [
+                {
+                    "_id": UUID("4198a349-0f53-4f48-a5ed-766517a26ef3"),
+                    "name": "Kiev"
+                },
+                {
+                    "_id": UUID("23fde963-54e5-4991-a0a9-d5013550703c"),
+                    "name": "Lviv"
+                }
+            ]
+    },
+    {
+        "_id": UUID("56346d81-b0ef-4e57-a18e-0b2ca3f04dd9"),
+        "country": "Russia",
+        "cities":
+            [
+                {
+                    "_id": UUID("ea88f894-a912-4a65-a823-392f1bbd514b"),
+                    "name": "Moscow"
+                },
+                {
+                    "_id": UUID("6224eb78-0d71-4299-951e-f756f6a824d2"),
+                    "name": "Saint-Petersburg"
+                }
+            ]
     }
-];
+]
