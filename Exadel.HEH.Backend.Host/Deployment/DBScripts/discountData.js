939538c6aeb16dc79442ea701a9fb6c351dda71d
YuriPetruchenia <yourockpet@gmail.com>
2021-02-07 19:40:01 +0300

    Little changes

--- Exadel.HEH.Backend.Host/Deployment/DBScripts/discountData.js
+++ Exadel.HEH.Backend.Host/Deployment/DBScripts/discountData.js
@@ -1,30 +1,42 @@
 ﻿let discountData = [
     {
         "_id": UUID("f1aac79e-f684-42f2-8ff9-8613b2541878"),
-        "conditions": "ConditionString",
+        "conditions": "30% by promocode",
         "tagsIds": [UUID("d1adffea-945d-44c6-a79d-b935ccec3051"),
         UUID("7de99c4f-d795-4a28-a7cf-1e2f952c71ee"),
         UUID("7bb365ae-87d7-458d-b170-00ceabea9c54")
         ],
         "vendorId": UUID("e7922459-d83c-4a8d-ba86-6b80e1e55d1d"),
-        "vendorName": "Food vendor",
-        "promoCode": "Eat",
+        "vendorName": "Mcdonalds",
+        "promoCode": "Mcfood",
         "addresses": [
             {
-                "_id": UUID("1323ad8f-eb0d-453a-aa85-4767d2a5d5eb"),
+                "_id": UUID("0a42474c-775f-43f4-8c7d-fd4873217de5"),
                 "countryId": UUID("0b343598-5efc-11eb-ae93-0242ac130002"),
                 "cityId": UUID("021e3664-df32-43f1-8851-f1449cfbc3dd"),
-                "street": "Shaliapina"
+                "street": "pr. Dzerzhinskogo, 96"
+            },
+            {
+                "_id": UUID("cf9e5eb1-5125-45ba-a22b-95d4d3ea121f"),
+                "countryId": UUID("56346d81-b0ef-4e57-a18e-0b2ca3f04dd9"),
+                "cityId": UUID("ea88f894-a912-4a65-a823-392f1bbd514b"),
+                "street": "ul. Nemiga, 12B"
+            },
+            {
+                "_id": UUID("231780e4-29e5-4572-aef9-24e6830a0f0d"),
+                "countryId": UUID("571ffd86-ae90-4457-b1ec-0ed504076706"),
+                "cityId": UUID("4198a349-0f53-4f48-a5ed-766517a26ef3"),
+                "street": "pr. Nezavisimosti, 23"
             }
         ],
         "phones": [
             {
-                "_id": UUID("15507b0f-201c-4072-a3a1-03d24dab27e8"),
-                "number": "88005553535"
+                "_id": UUID("75e228dc-ee5b-4d81-bf42-6133c5330774"),
+                "number": "+375172700047"
             },
             {
-                "_id": UUID("9e9f4beb-9b7b-4624-bf6b-b160e8d5fcf3"),
-                "number": "80299379992"
+                "_id": UUID("18f71e53-def0-4ae8-841c-75a9e871c347"),
+                "number": "+375172178471"
             }
         ],
         "startDate": "2021-02-03T18:30:25",
@@ -33,31 +45,27 @@
     },
     {
         "_id": UUID("509ce980-e780-42ca-a982-4d33c0b9dfa8"),
-        "conditions": "ConditionString2",
+        "conditions": "10% by promocode",
         "tagsIds": [
             UUID("3d9fb1b9-c326-4a85-986f-237cabc0e368"),
             UUID("45fd014b-1389-4036-824a-7dd11c43d1cd"),
             UUID("055a0b7f-dfed-4e7a-9ea4-0e380bb6dcfb")
         ],
         "vendorId": UUID("b883835e-4ce3-4134-9811-45a85b6ff113"),
-        "vendorName": "Health vendor",
-        "promoCode": "Health",
+        "vendorName": "Max Mirnyi Center",
+        "promoCode": "Tenis",
         "addresses": [
             {
-                "_id": UUID("27d0f2dd-127b-4bad-a9a4-d2466b060ab9"),
+                "_id": UUID("1da47e2f-ca87-4e9f-a905-023b0bc930ab"),
                 "countryId": UUID("0b343598-5efc-11eb-ae93-0242ac130002"),
                 "cityId": UUID("021e3664-df32-43f1-8851-f1449cfbc3dd"),
-                "street": "Pushkina"
+                "street": "ul. Gromova 14"
             }
         ],
         "phones": [
             {
-                "_id": UUID("288bb97b-e9fa-4e55-92fa-64cd0cff84c3"),
-                "number": "88005553535"
-            },
-            {
-                "_id": UUID("1cd60e33-71a0-4950-ab7d-5d75f95085da"),
-                "number": "80299379992"
+                "_id": UUID("0b89bcbc-6b14-48ce-8946-bb093fc853d8"),
+                "number": "+375293406506"
             }
         ],
         "startDate": "2021-10-03T18:30:25",
@@ -66,30 +74,26 @@
     },
     {
         "_id": UUID("d4ece352-2344-45c3-aa22-bd8ff94e5d7e"),
-        "conditions": "ConditionString3",
+        "conditions": "10% by promocode",
         "tagsIds": [
             UUID("bfd824c2-0f9d-4b76-848f-37828c161d9e"),
             UUID("6bf3f73e-c844-4cd5-a3bc-6f3364530468")
         ],
         "vendorId": UUID("a81d4813-0ff3-4a2b-b53c-ebec989ae7fc"),
-        "vendorName": "Health vendor",
-        "promoCode": "Health",
+        "vendorName": "Beautycenter O2",
+        "promoCode": "nails",
         "addresses": [
             {
-                "_id": UUID("bac73aae-e481-4b30-8972-0ae9d7b009b7"),
+                "_id": UUID("e52e7a46-08ad-400d-a475-2cf16a20c360"),
                 "countryId": UUID("0b343598-5efc-11eb-ae93-0242ac130002"),
                 "cityId": UUID("021e3664-df32-43f1-8851-f1449cfbc3dd"),
-                "street": "Lermontova"
+                "street": "ul.Zybitskaya 6-32"
             }
         ],
         "phones": [
             {
-                "_id": UUID("536512e5-2eb7-4895-830e-a1181e9224d7"),
-                "number": "88005553535"
-            },
-            {
-                "_id": UUID("5587b60e-e3b2-4e48-ba08-385c75874504"),
-                "number": "80299379992"
+                "_id": UUID("be7ef511-a3c6-4904-9d2f-5e5d58687bcb"),
+                "number": "+375298819819"
             }
         ],
         "startDate": "2021-10-03T18:30:25",
@@ -98,30 +102,26 @@
     },
     {
         "_id": UUID("e0d69154-4d2e-463d-b8e7-f497489e0751"),
-        "conditions": "ConditionString3",
+        "conditions": "15% by promocode",
         "tagsIds": [
             UUID("bb5fd66d-63d0-4dde-8580-7a26771bc129"),
             UUID("7f7b40be-7c47-4866-a867-b60504c4bb4f")
         ],
         "vendorId": UUID("91d19b85-6232-4373-b08b-a988d828d27e"),
-        "vendorName": "Entertainment vendor",
-        "promoCode": "Entertainment",
+        "vendorName": "Dinozavria",
+        "promoCode": "DinosorAndI",
         "addresses": [
             {
-                "_id": UUID("e6478320-f2d7-43d4-90cd-90eb71925989"),
+                "_id": UUID("2246d903-fcf2-45ca-8f9e-f9226fbe74c9"),
                 "countryId": UUID("0b343598-5efc-11eb-ae93-0242ac130002"),
                 "cityId": UUID("021e3664-df32-43f1-8851-f1449cfbc3dd"),
-                "street": "Zukova"
+                "street": "ul. Pritytskogo 29"
             }
         ],
         "phones": [
             {
-                "_id": UUID("1effe9fc-f789-4f1f-ad82-973a31cc81d0"),
-                "number": "254634756463"
-            },
-            {
-                "_id": UUID("6e880363-3d61-452f-8e2d-22fef2c123a3"),
-                "number": "14523468777777"
+                "_id": UUID("f6fd6b9b-53af-4ca2-a53d-18d551dd2045"),
+                "number": "+375445429429"
             }
         ],
         "startDate": "2021-10-03T18:30:25",
@@ -130,30 +130,38 @@
     },
     {
         "_id": UUID("ec82d67f-1371-4a69-98c9-ef425ba2e47d"),
-        "conditions": "ConditionString4",
+        "conditions": "3% by promocode",
         "tagsIds": [
             UUID("d1575ac0-cedf-49fd-9fb5-18579878720d"),
             UUID("76dce0ac-3d3d-49cd-8f15-fbe43ebe9900")
         ],
         "vendorId": UUID("34654c1a-a2c9-4562-abdd-98e62cbcc6f7"),
-        "vendorName": "Auto vendor",
+        "vendorName": "Autocenter Citroen",
         "promoCode": "Auto",
         "addresses": [
             {
-                "_id": UUID("fa4234c9-584f-4803-9d14-cabfd0cec552"),
-                "countryId": UUID("571ffd86-ae90-4457-b1ec-0ed504076706"),
-                "cityId": UUID("4198a349-0f53-4f48-a5ed-766517a26ef3"),
-                "street": "Zubrova"
+                "_id": UUID("f868212b-bbf3-4a68-a13c-7459b9eeeabb"),
+                "countryId": UUID("0b343598-5efc-11eb-ae93-0242ac130002"),
+                "cityId": UUID("021e3664-df32-43f1-8851-f1449cfbc3dd"),
+                "street": "ul. Kolesnikova 38"
+            },
+            {
+                "_id": UUID("0f45a222-af0a-4536-a88d-8f2b2a027fdd"),
+                "countryId": UUID("0b343598-5efc-11eb-ae93-0242ac130002"),
+                "cityId": UUID("c77e4c89-57f5-4204-8899-af47545cca22"),
+                "street": "ul. Kol'tsova 48"
+            },
+            {
+                "_id": UUID("d43680d8-9460-4fbe-ac99-c57f8fe7fdfa"),
+                "countryId": UUID("56346d81-b0ef-4e57-a18e-0b2ca3f04dd9"),
+                "cityId": UUID("ea88f894-a912-4a65-a823-392f1bbd514b"),
+                "street": "ul. Lazurnaya 13a"
             }
         ],
         "phones": [
             {
-                "_id": UUID("e53ad43a-8cbf-4594-bfd1-198a1be7e0cc"),
-                "number": "34586765343"
-            },
-            {
-                "_id": UUID("23d7b3f4-1c12-4cde-a6bc-ee9d21493925"),
-                "number": "09768090976"
+                "_id": UUID("dc9598e2-eb40-4bd7-ac31-1014900e72d8"),
+                "number": "+375173369955"
             }
         ],
         "startDate": "2021-10-03T18:30:25",
@@ -162,30 +170,20 @@
     },
     {
         "_id": UUID("3e387bce-a5e5-4a97-9647-6c5fa3645f1b"),
-        "conditions": "ConditionString5",
+        "conditions": "15% by promocode",
         "tagsIds": [
             UUID("0998631c-eb37-4767-8ff7-12bc8166dfae"),
             UUID("58e6f3d3-a1c4-488f-96a7-325824993816")
         ],
         "vendorId": UUID("72ff6421-f6c6-4d55-bf54-f9833723f30f"),
-        "vendorName": "Travel vendor",
+        "vendorName": "Booking",
         "promoCode": "Travel",
         "addresses": [
-            {
-                "_id": UUID("1b18c964-9e48-4a09-b4e4-25a008dbb987"),
-                "countryId": UUID("56346d81-b0ef-4e57-a18e-0b2ca3f04dd9"),
-                "cityId": UUID("ea88f894-a912-4a65-a823-392f1bbd514b"),
-                "street": "Poleskaia"
-            }
         ],
         "phones": [
             {
-                "_id": UUID("1c306030-953b-4dea-a430-f5dae2ac3ef5"),
-                "number": "921791985134"
-            },
-            {
-                "_id": UUID("b276b302-56de-4bde-95a3-6ce4f160c42b"),
-                "number": "9981247217821"
+                "_id": UUID("5071092e-e7b0-491f-813e-8113071015ab"),
+                "number": "+375296666666"
             }
         ],
         "startDate": "2021-10-03T18:30:25",
@@ -194,30 +192,26 @@
     },
     {
         "_id": UUID("b0697535-7071-4380-80d1-15556203dd0c"),
-        "conditions": "ConditionString6",
+        "conditions": "7% by promocode",
         "tagsIds": [
             UUID("42a69c2c-d3a0-4ec8-b0ef-5319bc27538f"),
             UUID("3d9590bb-49b6-4f08-b717-264c34ec6d26")
         ],
         "vendorId": UUID("b2f9bca3-bd3a-4fa1-874b-b17025a05873"),
-        "vendorName": "Auto vendor",
+        "vendorName": "It-academy",
         "promoCode": "Auto",
         "addresses": [
             {
-                "_id": UUID("bfefd5cd-b24f-4829-8dc8-04c3d050b96d"),
-                "countryId": UUID("56346d81-b0ef-4e57-a18e-0b2ca3f04dd9"),
-                "cityId": UUID("6224eb78-0d71-4299-951e-f756f6a824d2"),
-                "street": "NevskiiPrkt"
+                "_id": UUID("5fd5796b-a71b-415e-9492-169e16d7502d"),
+                "countryId": UUID("0b343598-5efc-11eb-ae93-0242ac130002"),
+                "cityId": UUID("021e3664-df32-43f1-8851-f1449cfbc3dd"),
+                "street": "ul. Skryganova 14"
             }
         ],
         "phones": [
             {
-                "_id": UUID("57ee5e8c-5651-4583-b3e5-3904d984fa3d"),
-                "number": "921791985134"
-            },
-            {
-                "_id": UUID("d4cdc0d4-91c0-40d3-8c12-307713c7043c"),
-                "number": "9981247217821"
+                "_id": UUID("9e5d1d27-3196-4102-818d-8c2c48bc428a"),
+                "number": "+375297815370"
             }
         ],
         "startDate": "2021-10-03T18:30:25",
