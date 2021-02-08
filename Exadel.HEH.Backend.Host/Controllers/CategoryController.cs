cc2e71ba1783cfe269a3b16f7e29ff1f0ec489ee
elizavetapavliv <pavliv.liza@yandex.ru>
2021-02-08 12:20:29 +0300

    Change token lifetime and add authorize to categories

--- Exadel.HEH.Backend.Host/Controllers/CategoryController.cs
+++ Exadel.HEH.Backend.Host/Controllers/CategoryController.cs
@@ -11,7 +11,7 @@ namespace Exadel.HEH.Backend.Host.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class CategoryController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
