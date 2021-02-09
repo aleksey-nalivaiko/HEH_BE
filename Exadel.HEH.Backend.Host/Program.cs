45fc7eefe00912335be2a11c9321bd5d9633a0fa
Juliya Terpilovskaya <juliya.tserpilouskaya@gmail.com>
2021-02-09 11:06:43 +0300

    + add serilog + add logging for exception middleware

--- Exadel.HEH.Backend.Host/Program.cs
+++ Exadel.HEH.Backend.Host/Program.cs
@@ -1,5 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Compact;
 
 namespace Exadel.HEH.Backend.Host
 {
@@ -7,11 +10,30 @@ namespace Exadel.HEH.Backend.Host
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(new RenderedCompactJsonFormatter())
+                .CreateLogger();
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Start failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+            .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
