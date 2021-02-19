00d98bb7617399dd6974f7d4c0b11c07582db88d
elizavetapavliv <pavliv.liza@yandex.ru>
2021-02-19 18:02:42 +0300

    Remove unused

--- Exadel.HEH.Backend.DataAccess/Models/GroupSumResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Exadel.HEH.Backend.DataAccess.Models
-{
-    public class GroupSumResult<T, TSum>
-    {
-        public T Key { get; set; }
-
-        public TSum Sum { get; set; }
-    }
-}
