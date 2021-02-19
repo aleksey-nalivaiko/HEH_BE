039e1320db0c1ec110a0b5328da27d24ed439b8a
KonstantinMarchenko <k_mark99@mail.ru>
2021-02-19 11:16:59 +0300

    Add statistics data

---
+++ Exadel.HEH.Backend.BusinessLogic/Mappings/StatisticsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Exadel.HEH.Backend.BusinessLogic.DTOs;
+using Exadel.HEH.Backend.DataAccess.Models;
+
+namespace Exadel.HEH.Backend.BusinessLogic.Mappings
+{
+    public class StatisticsProfile : Profile
+    {
+        public StatisticsProfile()
+        {
+            CreateMap<Statistics, StatisticsDto>().ReverseMap();
+        }
+    }
+}
