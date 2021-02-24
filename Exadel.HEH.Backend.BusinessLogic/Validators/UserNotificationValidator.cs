bf02dd8e773fd67ad97ed1f344058430c672eb53
elizavetapavliv <pavliv.liza@yandex.ru>
2021-02-24 19:44:10 +0300

    Fix notification validation

---
+++ Exadel.HEH.Backend.BusinessLogic/Validators/UserNotificationValidator.cs
@@ -0,0 +1,30 @@
+﻿using Exadel.HEH.Backend.BusinessLogic.DTOs;
+using Exadel.HEH.Backend.BusinessLogic.ValidationServices.Abstract;
+using FluentValidation;
+
+namespace Exadel.HEH.Backend.BusinessLogic.Validators
+{
+    public class UserNotificationValidator : AbstractValidator<UserNotificationDto>
+    {
+        public UserNotificationValidator(
+            ICategoryValidationService categoryValidationService,
+            ITagValidationService tagValidationService,
+            IVendorValidationService vendorValidationService)
+        {
+            RuleForEach(n => n.TagNotifications)
+                .MustAsync(tagValidationService.TagExistsAsync)
+                .WithMessage("Some of tags don't exist")
+                .When(n => n.TagNotifications != null);
+
+            RuleForEach(n => n.CategoryNotifications)
+                .MustAsync(categoryValidationService.CategoryExistsAsync)
+                .WithMessage("Some of categories don't exist")
+                .When(n => n.CategoryNotifications != null);
+
+            RuleForEach(n => n.VendorNotifications)
+                .MustAsync(vendorValidationService.VendorExistsAsync)
+                .WithMessage("Some of vendors don't exist")
+                .When(n => n.VendorNotifications != null);
+        }
+    }
+}
