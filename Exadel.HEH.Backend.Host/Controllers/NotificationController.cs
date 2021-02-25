966a55ab68f7b205169bfc3b7118501e83e1e3be
KonstantinMarchenko <k_mark99@mail.ru>
2021-02-25 11:41:24 +0300

    Notification validation on get

--- Exadel.HEH.Backend.Host/Controllers/NotificationController.cs
+++ Exadel.HEH.Backend.Host/Controllers/NotificationController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Exadel.HEH.Backend.BusinessLogic.DTOs;
 using Exadel.HEH.Backend.BusinessLogic.Services.Abstract;
+using Exadel.HEH.Backend.BusinessLogic.ValidationServices.Abstract;
 using Exadel.HEH.Backend.DataAccess.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,16 +15,24 @@ namespace Exadel.HEH.Backend.Host.Controllers
     public class NotificationController : ControllerBase
     {
         private readonly INotificationService _notificationService;
+        private readonly INotificationValidationService _notificationValidationService;
 
-        public NotificationController(INotificationService notificationService)
+        public NotificationController(INotificationService notificationService,
+            INotificationValidationService notificationValidationService)
         {
             _notificationService = notificationService;
+            _notificationValidationService = notificationValidationService;
         }
 
         [HttpGet("{id:guid}")]
-        public Task<NotificationDto> GetByIdAsync(Guid id)
+        public async Task<ActionResult<NotificationDto>> GetByIdAsync(Guid id)
         {
-            return _notificationService.GetByIdAsync(id);
+            if (await _notificationValidationService.NotificationExistsAsync(id))
+            {
+                return Ok(await _notificationService.GetByIdAsync(id));
+            }
+
+            return NotFound();
         }
 
         [HttpGet("count")]
