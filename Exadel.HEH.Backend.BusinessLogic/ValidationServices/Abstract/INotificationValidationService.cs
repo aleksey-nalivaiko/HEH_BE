c84d0cd03f2666228a75cfd02282da7eb5c20777
KonstantinMarchenko <k_mark99@mail.ru>
2021-02-25 11:41:24 +0300

    Notification validation on get

---
+++ Exadel.HEH.Backend.BusinessLogic/ValidationServices/Abstract/INotificationValidationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Exadel.HEH.Backend.BusinessLogic.ValidationServices.Abstract
+{
+    public interface INotificationValidationService
+    {
+        Task<bool> NotificationExistsAsync(Guid id);
+    }
+}
---
+++ Exadel.HEH.Backend.BusinessLogic/ValidationServices/NotificationValidationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Exadel.HEH.Backend.BusinessLogic.ValidationServices.Abstract;
+using Exadel.HEH.Backend.DataAccess.Repositories.Abstract;
+
+namespace Exadel.HEH.Backend.BusinessLogic.ValidationServices
+{
+    public class NotificationValidationService : INotificationValidationService
+    {
+        private readonly INotificationRepository _notificationRepository;
+
+        public NotificationValidationService(INotificationRepository notificationRepository)
+        {
+            _notificationRepository = notificationRepository;
+        }
+
+        public async Task<bool> NotificationExistsAsync(Guid id)
+        {
+            var result = await _notificationRepository.GetByIdAsync(id);
+
+            return result != null;
+        }
+    }
+}
