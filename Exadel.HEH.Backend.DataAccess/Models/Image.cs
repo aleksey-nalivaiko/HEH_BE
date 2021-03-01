df00b8cffb0ade61620dd91035ae9479827f450a
elizavetapavliv <pavliv.liza@yandex.ru>
2021-03-01 21:16:33 +0300

    Add methods for getting photo with caching

---
+++ Exadel.HEH.Backend.DataAccess/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace Exadel.HEH.Backend.DataAccess.Models
+{
+    public class Image
+    {
+        public string ContentType { get; set; }
+
+        public byte[] Content { get; set; }
+    }
+}
