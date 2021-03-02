b391ad69edb638b177d5052dfd52a8fffbee51f7
elizavetapavliv <pavliv.liza@yandex.ru>
2021-03-02 18:21:01 +0300

    User tests

--- Exadel.HEH.Backend.DataAccess.Tests/UserRepositoryTests.cs
+++ Exadel.HEH.Backend.DataAccess.Tests/UserRepositoryTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Exadel.HEH.Backend.DataAccess.Extensions;
 using Exadel.HEH.Backend.DataAccess.Models;
 using Exadel.HEH.Backend.DataAccess.Repositories;
+using Moq;
 using Xunit;
 
 namespace Exadel.HEH.Backend.DataAccess.Tests
@@ -41,6 +43,50 @@ namespace Exadel.HEH.Backend.DataAccess.Tests
                 },
                 Password = "abc"
             };
+
+            var usersWithSubscriptions = new List<User>();
+
+            Context.Setup(c => c.GetAnyInAndWhereAsync(It.IsAny<Expression<Func<User, IEnumerable<Guid>>>>(),
+                    It.IsAny<IEnumerable<Guid>>(), It.IsAny<Expression<Func<User, bool>>>()))
+                    .Callback((
+                        Expression<Func<User, IEnumerable<Guid>>> inField,
+                        IEnumerable<Guid> subscriptions,
+                        Expression<Func<User, bool>> expression) =>
+                    {
+                        var users = Collection.Where(expression.Compile()).ToList();
+                        var currentSubscriptions = users.Select(inField.Compile());
+                        var zipped = users.Zip(currentSubscriptions);
+
+                        foreach (var userSubscription in zipped)
+                        {
+                            if (userSubscription.Second.Any(id => subscriptions.ToList().Contains(id)))
+                            {
+                                usersWithSubscriptions.Add(userSubscription.First);
+                            }
+                        }
+                    })
+                    .Returns(Task.FromResult((IEnumerable<User>)usersWithSubscriptions));
+
+            Context.Setup(c => c.GetAnyEqAndWhereAsync(It.IsAny<Expression<Func<User, IEnumerable<Guid>>>>(),
+                    It.IsAny<Guid>(), It.IsAny<Expression<Func<User, bool>>>()))
+                .Callback((
+                    Expression<Func<User, IEnumerable<Guid>>> inField,
+                    Guid subscription,
+                    Expression<Func<User, bool>> expression) =>
+                {
+                    var users = Collection.Where(expression.Compile()).ToList();
+                    var currentSubscriptions = users.Select(inField.Compile());
+                    var zipped = users.Zip(currentSubscriptions);
+
+                    foreach (var userSubscription in zipped)
+                    {
+                        if (userSubscription.Second.Any(id => id == subscription))
+                        {
+                            usersWithSubscriptions.Add(userSubscription.First);
+                        }
+                    }
+                })
+                .Returns(Task.FromResult((IEnumerable<User>)usersWithSubscriptions));
         }
 
         [Fact]
@@ -77,6 +123,24 @@ namespace Exadel.HEH.Backend.DataAccess.Tests
 
             var result = await _repository.GetByEmail(_user.Email);
             Assert.Equal(_user, result);
+        }
+
+        [Fact]
+        public async Task CanGetWithSubscriptionsAsync()
+        {
+            Collection.Add(_user);
+            var result = await _repository.GetWithSubscriptionsAsync(u => u.VendorNotifications,
+                _user.VendorNotifications, u => u.IsActive);
+            Assert.Single(result);
+        }
+
+        [Fact]
+        public async Task CanGetWithSubscriptionAsync()
+        {
+            Collection.Add(_user);
+            var result = await _repository.GetWithSubscriptionAsync(u => u.VendorNotifications,
+                _user.VendorNotifications[0], u => u.IsActive);
+            Assert.Single(result);
         }
 
         [Fact]
