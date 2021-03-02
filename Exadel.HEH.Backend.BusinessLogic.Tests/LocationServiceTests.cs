2893a682a39bd124e078d87d240a81532749a999
elizavetapavliv <pavliv.liza@yandex.ru>
2021-03-02 20:21:40 +0300

    Add location services

--- Exadel.HEH.Backend.BusinessLogic.Tests/LocationServiceTests.cs
+++ Exadel.HEH.Backend.BusinessLogic.Tests/LocationServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Exadel.HEH.Backend.BusinessLogic.Services;
 using Exadel.HEH.Backend.DataAccess.Models;
@@ -34,15 +35,43 @@ namespace Exadel.HEH.Backend.BusinessLogic.Tests
             };
 
             locationRepository.Setup(r => r.GetAllAsync())
-                .Returns(() => Task.FromResult((IEnumerable<Location>)Data));
+                .Returns(Task.FromResult((IEnumerable<Location>)Data));
+
+            locationRepository.Setup(r => r.GetByIdsAsync(It.IsAny<IEnumerable<Guid>>()))
+                .Returns((IEnumerable<Guid> ids) => Task.FromResult(Data.Where(l => ids.Contains(l.Id))));
         }
 
         [Fact]
-        public async Task CanGetAll()
+        public async Task CanGetAllAsync()
         {
             Data.Add(_location);
             var result = await _service.GetAllAsync();
             Assert.Single(result);
+        }
+
+        [Fact]
+        public async Task CanGetByIdsAsync()
+        {
+            Data.Add(_location);
+
+            var location = new Location
+            {
+                Id = Guid.NewGuid(),
+                Country = "Country",
+                Cities = new List<City>
+                {
+                    new City
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "City"
+                    }
+                }
+            };
+
+            Data.Add(location);
+
+            var result = await _service.GetByIdsAsync(new List<Guid> { _location.Id, location.Id });
+            Assert.Equal(2, result.Count());
         }
     }
 }
