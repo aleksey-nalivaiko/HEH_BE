73f6bf723513ef758fd3b9be400fe743e3639b28
elizavetapavliv <pavliv.liza@yandex.ru>
2021-03-02 15:37:01 +0300

    Filtration in excel statistics

--- Exadel.HEH.Backend.BusinessLogic/Services/Abstract/IExportService.cs
+++ Exadel.HEH.Backend.BusinessLogic/Services/Abstract/IExportService.cs
@@ -1,14 +1,12 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Exadel.HEH.Backend.BusinessLogic.DTOs;
-using Microsoft.AspNet.OData.Query;
 
 namespace Exadel.HEH.Backend.BusinessLogic.Services.Abstract
 {
     public interface IExportService
     {
-        Task<MemoryStream> GetFileAsync(ODataQueryOptions<DiscountStatisticsDto> options,
-            string searchText = default, DateTime startDate = default, DateTime endDate = default);
+        Task<MemoryStream> GetFileAsync(IEnumerable<DiscountStatisticsDto> statistics);
     }
 }
