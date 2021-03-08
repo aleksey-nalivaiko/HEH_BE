88ed47a4e4205196625d120cf42a745146f77cfc
elizavetapavliv <pavliv.liza@yandex.ru>
2021-03-08 11:35:57 +0300

    Ping app

---
+++ Exadel.HEH.Backend.BusinessLogic/Options/PingOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Exadel.HEH.Backend.BusinessLogic.Options
+{
+    [ExcludeFromCodeCoverage]
+    public class PingOptions
+    {
+        public string PingEndpoint { get; set; }
+    }
+}
